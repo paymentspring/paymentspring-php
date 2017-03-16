1a94f040ae703d7e5a4c792b49cc0ecee50a1bbc
Noah Kochanowicz <noah@getlunchtray.com>
2017-03-16 18:51:19 -0400

    Changes directory and removes config vars

--- init.php
+++ init.php
@@ -1,9 +1,9 @@
 <?php
 
   // Initialize Payment Spring Singleton
-  require(dirname(__FILE__) . '/lib/PaymentSpring.php');
+  require(dirname(__FILE__) . '/payment-spring/PaymentSpring.php');
 
   // API Endpoints
-  require(dirname(__FILE__) . '/lib/Customer.php');
-  require(dirname(__FILE__) . '/lib/Charge.php');
-  require(dirname(__FILE__) . '/lib/Plan.php');
+  require(dirname(__FILE__) . '/payment-spring/Customer.php');
+  require(dirname(__FILE__) . '/payment-spring/Charge.php');
+  require(dirname(__FILE__) . '/payment-spring/Plan.php');
