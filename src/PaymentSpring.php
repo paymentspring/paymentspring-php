f8c6140e238ad52acb197e66837e87f8d0f76c5f
Noah Kochanowicz <noah@getlunchtray.com>
2017-04-04 19:06:15 -0400

    Comments code

--- src/PaymentSpring.php
+++ src/PaymentSpring.php
@@ -4,15 +4,32 @@ namespace PaymentSpring;
 
 class PaymentSpring {
 
+
+  /* Set the URL for PaymentSpring's API */
   public static $apiBaseURL = "https://api.paymentspring.com/api/v1/";
 
   public static $publicKey, $privateKey;
 
+  /*
+    * @param $publicKey string
+    * @param $privateKey string
+    * @return null; 
+  */
   public static function setApiKeys($publicKey, $privateKey){
     self::$publicKey = $publicKey;
     self::$privateKey = $privateKey;
   }
 
+  /*
+    *
+    * Base for all requests to PaymentSpring API
+    * Most functions using makeRequest will convert the response into a JSON object.
+    *
+    * @param $path string
+    * @param $params array optional 
+    * @param $isPost boolean optional 
+    * @return string
+  */
   public static function makeRequest($path, $params = array(), $isPost = false){
     $process = curl_init();
     $curlOptions = array(
@@ -28,6 +45,15 @@ class PaymentSpring {
     curl_close($process); 
     return $result;
   }
+
+  /*
+    *
+    * Adds headers for curl POST request 
+    *
+    * @param $options array 
+    * @param $params array 
+    * @return array 
+  */
   
   public static function constructPostRequest($options, $params){
     $options[CURLOPT_POST] = TRUE;
--- src/Plan.php
+++ src/Plan.php
@@ -2,16 +2,44 @@
   namespace PaymentSpring;
   
   class Plan {
+
+    /*
+      *
+      * List all plans
+      *
+      * @return Plan JSON Object 
+    */
+
     public static function listPlans(){
       $response = PaymentSpring::makeRequest("plans");
       return json_decode($response);
     } 
+
+    /*
+      *
+      * Subscribe a customer to a plan 
+      *
+      * @param $planId integer
+      * @param $customerId integer
+      * @param $options array optional 
+      * @return Subscription JSON Object 
+    */
 
     public static function subscribeCustomer($planId, $customerId, $options = array()){
       $requestPath = "plans/$planId/subscription/$customerId";
       $response = PaymentSpring::makeRequest($requestPath, $options, true);
       return json_decode($response);
     }
+
+    /*
+      *
+      * Create and subscribe a customer to a plan 
+      *
+      * @param $customerDetails array 
+      * @param $planId integer
+      * @param $options array optional 
+      * @return Customer JSON Object
+    */
 
     public static function createAndSubscribeCustomer($customerDetails, $planId, $options = array()){
       $customer = \PaymentSpring\Customer::createCustomer($customerDetails); 
